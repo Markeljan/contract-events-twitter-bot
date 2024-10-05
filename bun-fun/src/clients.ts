f2f239b8f62af26ca60bad86d656a8165af7f85e
Markeljan Sokoli <Markeljan19@gmail.com>
2024-10-05 12:08:38 -0400

    websockket reconnect options

--- bun-fun/src/clients.ts
+++ bun-fun/src/clients.ts
@@ -4,18 +4,36 @@ import { RPC_PROVIDER_API_KEY } from "@/config";
 
 export const publicClientArbitrum = createPublicClient({
   chain: arbitrum,
-  transport: http(`https://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`),
+  transport: http(
+    `https://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`
+  ),
 });
 export const webSocketClientArbitrum = createPublicClient({
   chain: arbitrum,
-  transport: webSocket(`wss://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`),
+  transport: webSocket(
+    `wss://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`,
+    {
+      keepAlive: true,
+      reconnect: true,
+      retryDelay: 1000,
+    }
+  ),
 });
 
 export const publicClientZkSync = createPublicClient({
   chain: zksync,
-  transport: http(`https://zksync-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`),
+  transport: http(
+    `https://zksync-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`
+  ),
 });
 export const webSocketClientZkSync = createPublicClient({
   chain: zksync,
-  transport: webSocket(`wss://zksync-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`),
+  transport: webSocket(
+    `wss://zksync-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`,
+    {
+      keepAlive: true,
+      reconnect: true,
+      retryDelay: 1000,
+    }
+  ),
 });
